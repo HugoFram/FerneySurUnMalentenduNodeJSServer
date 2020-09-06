b32e7d12e39913231eb6a96445762d0a65f66ee5
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-06 22:55:02 +0200

    Connect to Postgres DB + implement player GET.

---
+++ dbconnect.js
@@ -0,0 +1,15 @@
+var config = require('./config');
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+    user: config.user,
+    host: config.host,
+    database: config.dbname,
+    password: config.password,
+    port: config.port,
+  });
+  client.connect()
+    .then((db) => console.log("Connected correctly to the database"))
+    .catch((err) => console.log(err));
+
+module.exports = client;
