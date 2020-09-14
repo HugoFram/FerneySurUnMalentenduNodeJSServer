be9afc3edddb1ec3c7aa015b933df493f867eca4
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-14 13:52:18 +0200

    Update GET request handler for locations to match the client data model.

--- routes/location.js
+++ routes/location.js
@@ -10,7 +10,7 @@ locationRouter.use(bodyParser.json());
 locationRouter.route('/')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
     .get(cors.cors, (req, res, next) => {
-        client.query("SELECT * FROM location")
+        client.query("SELECT adress as address, longitude, latitude FROM location")
             .then(result => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', "application/json");
