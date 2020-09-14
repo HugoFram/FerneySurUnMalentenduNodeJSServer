c3e780f069f2b582e3bd7f86b5c7bc5f3157ff13
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-14 12:31:47 +0200

    Update GET request handler for ranking to match the client data model.

--- routes/ranking.js
+++ routes/ranking.js
@@ -10,7 +10,10 @@ rankingRouter.use(bodyParser.json());
 rankingRouter.route('/')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
     .get(cors.cors, (req, res, next) => {
-        client.query("SELECT * FROM ranking")
+        client.query('SELECT CAST(rank AS integer), team, CAST(score AS integer), \
+        (CAST(coalesce("vPlus", \'0\') AS integer) + CAST(coalesce("vMinus", \'0\') AS integer)) as wins, (CAST(coalesce("dPlus", \'0\') AS integer) + CAST(coalesce("dMinus", \'0\') AS integer)) as losses, \
+                    CAST(substring(sets from \'^[0-9]+\') AS integer) as "setsWon", CAST(substring(sets from \'[0-9]+$\') AS integer) as "setsLost", \
+                    CAST(substring(points from \'^[0-9]+\') AS integer) as "pointsWon", CAST(substring(points from \'[0-9]+$\') AS integer) as "pointsLost", CAST(matchs AS integer) as "numMatches" FROM ranking')
             .then(result => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', "application/json");
