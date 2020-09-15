abea5afdf75c4bfe9968b52f5476571bd043fbb7
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-16 00:48:16 +0200

    Update API to handle POST and DELETE request on the training-presences/:trainingDate endpoint.

--- routes/training-presence.js
+++ routes/training-presence.js
@@ -17,6 +17,35 @@ trainingPresenceRouter.route('/')
                 res.json(result.rows);
             })
             .catch(err => console.error(err.stack))
+    })
+
+trainingPresenceRouter.route('/:trainingDate')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .post(cors.corsWithOptions, (req, res, next) => {
+        console.log(req.body);
+        var query = "DELETE FROM training WHERE date = '" + req.params.trainingDate + "'; INSERT INTO training(name, date, presence) VALUES";
+        req.body.forEach((presence, i) => {
+            query = query + "('" + presence.name + "', '" + presence.date + "', '" + presence.presence + "')" + ((i == req.body.length - 1) ? "" : ",");
+        });
+        console.log(query);
+        client.query(query)
+            .then(result => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', "application/json");
+                console.log(result);
+                res.json(result);
+            })
+            .catch(err => console.error(err.stack))
+    })
+    .delete(cors.corsWithOptions, (req, res, next) => {
+        client.query("DELETE FROM training WHERE date = $1", [ req.params.trainingDate ])
+            .then(result => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', "application/json");
+                console.log(result);
+                res.json(result);
+            })
+            .catch(err => console.error(err.stack))
     });
 
 module.exports = trainingPresenceRouter;
