79cc7ff9fc79f0c71cbdf3bca873311978faa591
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-18 02:23:36 +0200

    Update API to handle POST and DELETE request on the match-presences/:matchDate endpoint.

--- routes/match-presence.js
+++ routes/match-presence.js
@@ -19,4 +19,39 @@ matchPresenceRouter.route('/')
             .catch(err => console.error(err.stack))
     });
 
+matchPresenceRouter.route('/:matchDate')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .post(cors.corsWithOptions, (req, res, next) => {
+        console.log(req.body);
+        var query = "DELETE FROM match WHERE date = '" + req.params.matchDate + "'; INSERT INTO match(name, date, presence, adversary, \"setsWon\", \"setsLost\") VALUES";
+        req.body.forEach((presence, i) => {
+            query = query + "('" + 
+                presence.name + "', '" + 
+                presence.date + "', '" + 
+                presence.presence + "', '" + 
+                presence.adversary + "', '" + 
+                presence.setsWon + "', '" + 
+                presence.setsLost + "')" + ((i == req.body.length - 1) ? "" : ",");
+        });
+        console.log(query);
+        client.query(query)
+            .then(result => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', "application/json");
+                console.log(result);
+                res.json(result);
+            })
+            .catch(err => console.error(err.stack))
+    })
+    .delete(cors.corsWithOptions, (req, res, next) => {
+        client.query("DELETE FROM match WHERE date = $1", [ req.params.matchDate ])
+            .then(result => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', "application/json");
+                console.log(result);
+                res.json(result);
+            })
+            .catch(err => console.error(err.stack))
+    });
+
 module.exports = matchPresenceRouter;
