d027de1007e49ef06421e899ca978a0f86856470
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-23 23:37:23 +0200

    Update API to handle POST request on the availabilities/:matchNum/:playerName endpoint.

--- routes/availability.js
+++ routes/availability.js
@@ -19,4 +19,28 @@ availabilityRouter.route('/')
             .catch(err => console.error(err.stack))
     });
 
+availabilityRouter.route('/:matchNum/:playerName')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .post(cors.corsWithOptions, (req, res, next) => {
+        console.log(req.body);
+        var query = "DELETE FROM availability WHERE \"matchNum\" = '" + req.params.matchNum + "' AND name = '" + req.params.playerName + "';";
+        query += "INSERT INTO availability(name, availability, role, \"trainingPresence\", \"matchPresence\", \"selected\", \"matchNum\") VALUES ('";
+        query += req.body.name + "', '" + 
+                 req.body.availability + "', '" + 
+                 req.body.role + "', '" + 
+                 req.body.trainingPresence + "', '" + 
+                 req.body.matchPresence + "', '" + 
+                 req.body.selected + "', '" + 
+                 req.body.matchNum + "');";
+        console.log(query);
+        client.query(query)
+            .then(result => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', "application/json");
+                console.log(result);
+                res.json(result);
+            })
+            .catch(err => console.error(err.stack))
+    });
+
 module.exports = availabilityRouter;
