6f7532091e68d3f6bec827084faf50b9a9a13d33
Hugo Frammery <hugo.frammery@gtherapeutics.com>
2020-09-23 18:36:47 +0200

    Update API to handle GET request on the player/:playerName endpoint to retrieve information of a single player.

--- routes/player.js
+++ routes/player.js
@@ -19,4 +19,16 @@ playerRouter.route('/')
             .catch(err => console.error(err.stack))
     });
 
+playerRouter.route('/:playerName')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
+    .get(cors.cors, (req, res, next) => {
+        client.query("SELECT * FROM player WHERE \"firstname\" = $1", [ req.params.playerName ])
+            .then(result => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', "application/json");
+                res.json(result.rows[0]);
+            })
+            .catch(err => console.error(err.stack))
+    });
+
 module.exports = playerRouter;
