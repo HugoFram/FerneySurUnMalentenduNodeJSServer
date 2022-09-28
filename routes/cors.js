489bd30dafbea507ef00a7adff622b25139c2d31
Hugo Frammery <hugo.frammery@kitro.ch>
2022-09-28 14:12:37 +0200

    Update IP in CORS policy.

--- routes/cors.js
+++ routes/cors.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200', 'http://217.162.196.67:' + config.clientport, 'http://192.168.0.157:' + config.clientport];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:4200', 'http://217.162.33.233:' + config.clientport, 'http://192.168.0.157:' + config.clientport];
 
 var corsOptionDelegate = (req, callback) => {
     var corsOptions;
